bb609bc9ea90759b4c91d7738bd519d5cefd3e51
jgrissom <jgrissom91@gmail.com>
2024-08-26 09:19:53 -0500

    [1.06] Write data to text file

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ if (resp == "1")
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
     // random number generator
     Random rnd = new();
+    // create file
+    StreamWriter sw = new("data.txt");
 
     // loop for the desired # of weeks
     while (dataDate < dataEndDate)
@@ -33,10 +35,12 @@ if (resp == "1")
             hours[i] = rnd.Next(4, 13);
         }
         // M/d/yyyy,#|#|#|#|#|#|#
-        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
         // add 1 week to date
         dataDate = dataDate.AddDays(7);
     }
+    sw.Close();
 }
 else if (resp == "2")
 {
